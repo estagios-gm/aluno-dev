a6d2f1664804eeaa47c340f5275eb4e5c10abeb5
Jabes <jabesr@gmail.com>
2022-11-30 16:27:16 -0300

    Updates

---
+++ precache-manifest.286479719c08041118cd05c9b7985797.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f21a4ecad0dd7946dc8a",
+    "url": "./static/js/main.088e0590.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b4ceaa597b4fa97dc2e6",
+    "url": "./static/js/2.cccfe27b.chunk.js"
+  },
+  {
+    "revision": "329f2083bc195cd05d5595da32cdd665",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.161156951b7864493e476290d8238355.js"
+  "./precache-manifest.286479719c08041118cd05c9b7985797.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("./index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
